5f34271fd19bdb30a3e7bb5252223ad0565d6661
everzhong <everzhong@163.com>
2021-06-12 17:15:25 +0800

    uu

--- src/views/system/dept/listd.vue
+++ src/views/system/dept/listd.vue
@@ -144,7 +144,7 @@ import Treeselect from "@riophae/vue-treeselect";
 import "@riophae/vue-treeselect/dist/vue-treeselect.css";
 
 export default {
-  name: "Dept",
+  name: "Listd",
   components: { Treeselect },
   data() {
     return {
